b13b2197a1929839b1bc4c07f5fe54ecce5a7c02
Elton Kuzniewski <kuzniewski@gmail.com>
2022-03-01 15:44:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (() => {
   window.setInterval(function () {
+    // folha
     let paywallScreen = document.getElementById('paywall-screen');
     let paywallFill = document.getElementById('paywall-fill');
     let paywallContent = document.getElementById('paywall-content');
@@ -7,6 +8,10 @@
     if (paywallScreen) paywallScreen.remove();
     if (paywallFill) paywallFill.remove();
     if (paywallContent) paywallContent.style.overflow = 'scroll';
+    
+    // oglobo
+    document.getElementsByClassName('paywall-cpt paywall-cpt-blogs')[0].remove(document.getElementsByClassName('paywall-cpt paywall-cpt-blogs')[0]);
+    document.getElementsByTagName('body')[0].style = "overflow: scroll !important; width: 100% !important;"
   }, 3000);
   
 })();
